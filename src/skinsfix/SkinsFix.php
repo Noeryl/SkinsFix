13c44c63a5d672953530bfc895e5465d19f22e58
Noeryl <25102019titan@gmail.com>
2026-05-06 14:50:24 +0100

    Initial plugin

---
+++ src/skinsfix/SkinsFix.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace skinsfix;
+
+use pocketmine\entity\Skin;
+use pocketmine\network\mcpe\convert\TypeConverter;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+use Ramsey\Uuid\Uuid;
+use skinsfix\skin\SkinAdapterPersona;
+use function chr;
+use function imagecolorat;
+use function imagecreatefrompng;
+use function imagesx;
+use function imagesy;
+
+final class SkinsFix extends PluginBase{
+
+    use SingletonTrait;
+
+    protected function onLoad() : void{
+        self::setInstance($this);
+
+        TypeConverter::getInstance()->setSkinAdapter(new SkinAdapterPersona());
+    }
+
+    public function getSteveSkin(string $capeData) : Skin{
+        $image = imagecreatefrompng($this->getResourceFolder(). "steve.png");
+
+        $skinData = "";
+        for($y = 0; $y < imagesy($image); $y++){
+            for($x = 0; $x < imagesx($image); $x++){
+                $rgba = imagecolorat($image, $x, $y);
+                $a = ((~($rgba >> 24)) << 1) & 0xff;
+                $r = ($rgba >> 16) & 0xff;
+                $g = ($rgba >> 8) & 0xff;
+                $b = $rgba & 0xff;
+                $skinData .= chr($r) . chr($g) . chr($b) . chr($a);
+            }
+        }
+
+        return new Skin(Uuid::uuid4()->toString(), $skinData, $capeData, "geometry.humanoid.custom");
+    }
+}
---
+++ src/skinsfix/skin/SkinAdapterPersona.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace skinsfix\skin;
+
+use pocketmine\network\mcpe\convert\LegacySkinAdapter;
+use pocketmine\network\mcpe\protocol\types\skin\SkinData;
+use pocketmine\entity\Skin;
+use pocketmine\entity\InvalidSkinException;
+use skinsfix\SkinsFix;
+use function explode;
+use function is_array;
+use function is_string;
+use function json_decode;
+
+final class SkinAdapterPersona extends LegacySkinAdapter{
+
+    public function fromSkinData(SkinData $data) : Skin{
+        $capeData = $data->getCapeImage()->getData();
+        $geometryName = explode(".", json_decode($data->getResourcePatch(), true)["geometry"]["default"]);
+        if(!isset($geometryName[2])){
+            return SkinsFix::getInstance()->getSteveSkin($capeData);
+        }
+
+        $patch = json_decode($data->getResourcePatch(), true);
+        if(is_array($patch) && isset($patch["geometry"]["default"]) && is_string($patch["geometry"]["default"])){
+            $geometryName = $patch["geometry"]["default"];
+        } else {
+            throw new InvalidSkinException("Missing geometry name field");
+        }
+
+        return new Skin($data->getSkinId(), $data->getSkinImage()->getData(), $capeData, $geometryName, $data->getGeometryData());
+    }
+}
